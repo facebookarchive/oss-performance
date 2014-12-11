78a3329ff0d6a4c1726dcf2874e062b3a780084e
Joel Pobar <joelpobar@gmail.com>
2014-12-11 14:00:46 -0800

    Adding a basic MediaWiki (https://www.mediawiki.org/) example.
    This adds MediaWiki as a target and creates a basic workload
    (Barack Obama and HHVM wiki pages: reading, printing, edit etc).

---
+++ targets/mediawiki/MediaWikiTarget.php
@@ -0,0 +1,37 @@
+<?hh
+/*
+ *  Copyright (c) 2014, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+final class MediaWikiTarget extends PerfTarget {
+  public function __construct(
+    private PerfOptions $options,
+  ) {
+  }
+
+  protected function getSanityCheckString(): string {
+    return 'Obama';
+  }
+
+  public function install(): void {
+    Utils::ExtractTar(
+      __DIR__.'/mediawiki-1.24.0.tar.gz',
+      $this->options->tempDir,
+    );
+
+    (new DatabaseInstaller($this->options))
+      ->setDatabaseName('mw_bench')
+      ->setDumpFile(__DIR__.'/mw_bench.sql.gz')
+      ->installDatabase();
+  }
+
+  public function getSourceRoot(): string {
+    return $this->options->tempDir.'/mediawiki';
+  }
+}
